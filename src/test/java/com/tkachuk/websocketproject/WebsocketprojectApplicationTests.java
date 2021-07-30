3095d19537986172dc9834bdfd6e1b55c9daf1cc
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-07-30 19:46:54 +0300

    add dependencies

---
+++ src/test/java/com/tkachuk/websocketproject/WebsocketprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkachuk.websocketproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
