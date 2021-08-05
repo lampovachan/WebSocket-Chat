cf0f803852e2a5d06116b0e19edf44f27adffcb7
Svitlana Tkachuk <misstkachuk15@gmail.com>
2021-08-05 11:40:26 +0300

    make changes in properties

--- src/main/java/com/tkachuk/websocketproject/WebsocketprojectApplication.java
+++ src/main/java/com/tkachuk/websocketproject/WebsocketprojectApplication.java
@@ -2,9 +2,10 @@ package com.tkachuk.websocketproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsocketprojectApplication {
+public class WebsocketprojectApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebsocketprojectApplication.class, args);
